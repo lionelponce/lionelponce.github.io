e0fbd809fea9faaa4f016e52bd4cf4b627f0732f
Lionel Ponce <lionel.ponce@outlook.com.ar>
2023-03-19 02:01:13 -0300

    arreglo flex en skills y agregado de  whatsapp

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 "use strict"
+document.getElementById('btnSend').addEventListener("click", sendWhatsapp);
+
+
+function sendWhatsapp(){
+    let name = encodeURIComponent(document.getElementById('name').value);
+    let message = encodeURIComponent(document.getElementById('message').value);
+
+    // https://wa.me/1XXXXXXXXXX?text=I'm%20interested%20in%20your%20car%20for%20sale
+    window.open("https://wa.me/+543465409011?text="+message+"%0A*"+name+"*");
+}
